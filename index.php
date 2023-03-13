32bac1b53b840de4d561a0115bd8cdc606ced3ed
tadziad <tad.ziad1@gmail.com>
2023-03-13 16:23:27 +0100

    add

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+echo "test";
+class MathOperations {
+    
+    public function add($a, $b) {
+        return $a + $b;
+    }
+    
+    public function subtract($a, $b) {
+        return $a - $b;
+    }
+    
+    public function multiply($a, $b) {
+        return $a * $b;
+    }
+    
+    public function divide($a, $b) {
+        if ($b == 0) {
+            throw new Exception('Division by zero');
+        }
+        return $a / $b;
+    }
+    
+}
+
+?>
---
+++ tests/MonTest.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('index.php');
+
+use PHPUnit\Framework\TestCase;
+
+class TestMathOperations extends TestCase {
+    
+    public function testAdd() {
+        $math = new MathOperations();
+        $this->assertEquals(4, $math->add(2, 2));
+        $this->assertEquals(-1, $math->add(2, -3));
+    }
+    
+    public function testSubtract() {
+        $math = new MathOperations();
+        $this->assertEquals(0, $math->subtract(2, 2));
+        $this->assertEquals(5, $math->subtract(2, -3));
+    }
+    
+    public function testMultiply() {
+        $math = new MathOperations();
+        $this->assertEquals(6, $math->multiply(2, 3));
+        $this->assertEquals(-6, $math->multiply(2, -3));
+    }
+    
+    public function testDivide() {
+        $math = new MathOperations();
+        $this->assertEquals(2, $math->divide(6, 3));
+        $this->assertEquals(-2, $math->divide(6, -3));
+        $this->expectException(Exception::class);
+        $math->divide(6, 0);
+    }
+    
+}
+
+?>
